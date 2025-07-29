0367c469665d0015264d201de4ef33df06b09207
kakaijiro <kakaijiro@gmail.com>
2025-07-29 16:46:09 +0900

    feat: add getProperties function and PropertyStatus type for property management

--- app/admin-dashboard/page.tsx
+++ app/admin-dashboard/page.tsx
@@ -1,9 +1,12 @@
 import { Breadcrumbs } from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
+import { getProperties } from "@/data/properties";
 import { PlusCircleIcon } from "lucide-react";
 import Link from "next/link";
 
-export default function Page() {
+export default async function Page() {
+  const data = await getProperties();
+  console.log({ data });
   return (
     <div>
       <Breadcrumbs
